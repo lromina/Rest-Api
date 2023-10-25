802c57be58a05a180c6e7390763b211276ca55d2
lromina <lromina01@gmail.com>
2023-10-25 18:48:52 -0300

    Error atributos inexistentes

--- src/controller.js
+++ src/controller.js
@@ -9,40 +9,69 @@ class LibroController{
     //Añadir un Nuevo Libro
     async add(req, res){
         const libro = req.body; // permite obtener la solicitud de la carga de usuario
+        const schema = {
+            nombre: String,
+            autor: String,
+            categoria: String,
+            anioPublicacion: Date,
+            ISBN: String,
+          
+        };
+        
         try {
-            const [result] = await pool.query (`INSERT INTO libros (nombre, autor, categoria, anioPublicacion, ISBN) VALUES (?,?,?,?,?)`, [libro.nombre, libro.autor, libro.categoria, libro.anioPublicacion, libro.ISBN]);
-            res.json({"Id Insertado": result.insertId});
+          // Verifica si los atributos de la solicitud coinciden con el schema
+            for (const atributo in libro) {
+              if (!schema.hasOwnProperty(atributo)) {
+              // El atributo no existe en el schema
+                res.status(400).json({ Error: `El atributo ${atributo} no existe` });
+                return;
+              }
+            }
+
+          const [existeLibro] = await pool.query(
+            `SELECT * FROM libros WHERE ISBN = ?`,[libro.ISBN]);
+
+            if (existeLibro.length > 0) {
+              res.status(409).json({ Error: 'El libro ya existe en la base de datos' });
+          } else {
+              // Si no se encontró un registro existente, realiza la inserción
+              const [result] = await pool.query(
+                  'INSERT INTO libros (nombre, autor, categoria, anioPublicacion, ISBN) VALUES (?, ?, ?, ?, ?)',
+                  [libro.nombre, libro.autor, libro.categoria, libro.anioPublicacion, libro.ISBN]
+              );
+              res.json({ "Id Insertado": result.insertId });
+          }
+      } catch (error) {
+          res.status(500).json({ Error: 'Error en los datos de la base' });
+      }
+
+    }
+    
+    //Eliminar un Registro
+    async delete(req, res){
+        const libro = req.body;
+        try {
+          const [result] = await pool.query (`DELETE FROM libros WHERE ISBN=(?)`, [libro.ISBN]);
+          if (result.length > 0) {
+            res.json({"Registro Eliminado": result.affectedRows});
+          } else {
+            res.status(404).json({ Error: 'Libro no encontrado', libro});
+          }
+          
         } catch (error) {
-            res.status(404).json({ Error: 'Datos Inexistentes o Duplicados', Id: libro});
-
-
-            //if (error instanceof TypeError) {
-              //  console.log("Error de Tipo")
-            //}
-            //if (error instanceof SyntaxError) {
-              //  console.log("Error de Sintaxis")
-            //}
-            //if (error instanceof ReferenceError) {
-              //  console.log("Error de Referencia")
-            //}
-            
-        }
+          res.status(404).json({ Error: 'No es posible eliminar el Registro', Id: libro});
+        } 
         
     }
 
-
-    async delete(req, res){
-        const libro = req.body; 
-        const [result] = await pool.query (`DELETE FROM libros WHERE ISBN=(?)`, [libro.ISBN]);
-        res.json({"Registro Eliminado": result.affectedRows});
-    }
-
+    //Modificar un Registro
     async update(req, res){
         const libro = req.body; 
         const [result] = await pool.query (`UPDATE libros SET nombre =(?), autor=(?), categoria=(?), anioPublicacion=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.anioPublicacion, libro.ISBN, libro.id]);
         res.json({"Registro Modificado": result.changedRows});
     }
 
+    //Obtener un Registro
     async getOne(req, res) {
         const libro = req.params.id; // Obtenemos el ID de la URL
         //const libro = req.body.id; // obtenemos el Id cargando en el body
@@ -65,5 +94,14 @@ export const libro = new LibroController();
 
 
 
-
+  //if (error instanceof TypeError) {
+              //  console.log("Error de Tipo")
+            //}
+            //if (error instanceof SyntaxError) {
+              //  console.log("Error de Sintaxis")
+            //}
+            //if (error instanceof ReferenceError) {
+              //  console.log("Error de Referencia")
+            //}
+            
   
